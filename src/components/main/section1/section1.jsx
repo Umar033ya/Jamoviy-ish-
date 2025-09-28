35ec2989adadc51a5ba886272a415a8fe48c3c50
Umar033ya <umaryakubov033ppp@gmail.com>
2025-09-28 13:05:57 +0500

    stn1 and stn2 add

--- src/components/main/section1/section1.jsx
+++ src/components/main/section1/section1.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import './section1.css'
+import stn1Bg from '../../../../public/section1Bg.png'
 
 function Section1() {
 
   return (
     <div className='body'>
-      <h1>salom</h1>
+      <section className='section1' style={{
+         backgroundImage: `url(${stn1Bg})`,
+         backgroundSize: "cover",
+         backgroundPosition: "center",
+         height: "90vh"
+      }}>
+        <h4>Food app</h4>
+        <h1>Why stay hungry when you can order form Bella Onojie</h1>
+        <p>Download the bella onoje’s food app now on</p>
+        <div className="btnParent">
+          <button>Playstore</button>
+          <button>App store</button>
+        </div>
+      </section>
     </div>
   )
 }
--- src/components/main/section2/section2.jsx
+++ src/components/main/section2/section2.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './section2.css'
+import stn2 from '../../../../public/section2.png'
 
 function Section2() {
 
   return (
     <div className='body'>
-      <h1>salom</h1>
+        <section className='section2'>
+          <img src={stn2} alt="" />
+        </section>
     </div>
   )
 }
