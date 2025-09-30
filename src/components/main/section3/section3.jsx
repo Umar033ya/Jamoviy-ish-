617d45a0502bced070484382528fa93e460c1a34
asilushko <sajfuddinovasliddin35@gmail.com>
2025-09-30 17:58:21 +0500

    stn3 stn4 add

--- src/components/main/section3/section3.jsx
+++ src/components/main/section3/section3.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import './section3.css'
-
+import img5 from '../../../../public/kok.png'
 function Section3() {
 
   return (
     <div className='body'>
-      <h1>salom</h1>
+ 
+<section className='ko' >
+    <div className='ji' >
+                    <h3>Create an account</h3>
+    <h2>Create/login to an existing
+account to get started</h2>
+<p>An account is created with your email
+and a desired password</p>
+          </div>
+      <img src={img5} alt="" />
+</section>
+
     </div>
   )
 }
--- src/components/main/section4/section4.jsx
+++ src/components/main/section4/section4.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import './section4.css'
-
+import img6 from '../../../../public/koku.png'
 function Section4() {
 
   return (
     <div className='body'>
-            <h1>salom</h1>
+          <section className='oiu' >
+    <div className='iou'>
+      <img src={img6} alt="" />
+          <div className='ji' >
+                    <h3>Create an account</h3>
+    <h2>Create/login to an existing
+account to get started</h2>
+<p>An account is created with your email
+and a desired password</p>
+          </div>
+
+    </div>
+          </section>
     </div>
   )
 }
