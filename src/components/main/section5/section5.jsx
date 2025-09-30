24ab9472f714d834ac6415eb64131e8f18b105db
Umar033 <umaryakubov033ppp@gmail.com>
2025-09-30 18:04:12 +0500

    Update section5.jsx
    oz'gartirdim image rasmni

--- src/components/main/section5/section5.jsx
+++ src/components/main/section5/section5.jsx
@@ -7,15 +7,15 @@ function Section5() {
   return (
     <div className="body">
           <div className="section5">
-        <div className="sect5-left">
-          <img src={payment} alt="rasm" />
-        </div>
         <div className="sect5-right">
           <p className='sect5-right-p1'>Checkout</p>
           <h3>When you done check out
           and get it delivered.</h3>
           <p className='sect5-right-p2'>When you done check out and get it 
           delivered with ease.</p>
+        </div>
+            <div className="sect5-left">
+          <img src={payment} alt="rasm" />
         </div>
       </div>
     </div>
