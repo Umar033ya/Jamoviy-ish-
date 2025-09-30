22a06f1d2cbc29cbe77d02b18d3fbecdc04dadb9
woxx11 <sheikh07wz@gmail.com>
2025-09-30 17:43:55 +0500

    vorzakon

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import './header.css'
+import "./header.css";
+import left22 from "../../assets/images/group.png";
 
 function Header() {
-
   return (
-    <div className='body'>
-      <h1>salom</h1>
+    <div className="body">
+      <header>
+        <div className="left">
+          <img src={left22} alt="" />
+        </div>
+        <div className="right">
+          <ul>
+            <li>
+              <a href="#">Home</a>
+              <a href="#">Product</a>
+              <a href="#">Faq</a>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </header>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
